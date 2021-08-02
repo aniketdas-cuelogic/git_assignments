018a11727ba5ed0ee5603bb3a9556be846380eaf
Aniket Das <aniket.das@cuelogic.com>
2021-08-02 15:46:10 +0530

    Addition.py file added

---
+++ Addition.py
@@ -0,0 +1,8 @@
+def add(n1,n2):
+    sum = float(n1) + float(n2)
+    return sum;
+    
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+print("The sum of two number is  ", add(num1,num2))
